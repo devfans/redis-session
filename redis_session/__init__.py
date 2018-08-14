4d35ed28b0e3c83fea9be8416a6c65c86c4dd3c1
Stefan Liu <stefanliu@outlook.com>
2018-08-14 19:08:07 +0800

    version tick

--- redis_session/__init__.py
+++ redis_session/__init__.py
@@ -3,7 +3,7 @@
 
 from redis_session.session import SessionHandler
 
-__VERSION__ = '0.0.2'
-version_info = (0, 0, 2)
+__VERSION__ = '0.0.3'
+version_info = (0, 0, 3)
 
 __all__ = ['SessionHandler']
--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import redis_session
+import redis_session, os
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+long_description = f.read()
+f.close()
+
 setup(
     name='redis_session',
     version=redis_session.__VERSION__,
     description="Simple session implementation for Tornado",
+    long_description=long_description,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Topic :: Internet :: WWW/HTTP :: Session',
